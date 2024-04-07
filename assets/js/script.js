c6380df64d56694d42c1d1233e19d813e1132978
Code-with-Nikhil-07 <144557756+coder-nikhil-07@users.noreply.github.com>
2024-04-07 16:00:05 +0530

    script update

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
 
     // navigation open js start
     $('.toggle-switch').click(function () {
-        $('.header-inner-wrapper').addClass('header-active')
+        $('.header-inner-wrapper').toggleClass('header-active')
     });
 });
 
 // Navbar close js start
 function navbar_close() {
     document.querySelector('.menu ul').style.display = 'none';
-}
+};
 // Assuming you have a button with the class 'remove-button'
 var removeButton = document.querySelector('.close-menu-icon');
 var headerInnerWrapper = document.querySelector('.header-inner-wrapper');
@@ -70,13 +70,37 @@ function cv() {
     file_d.setAttribute('href', file);
 }
 
+// Get contact form data with use js
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyFJ8Ru8lAVkAGvPea5jQOV7ksCxAIT0zjifm_a_uehuwIK7THsA-ebZKw7_rVDFBG3/exec';
+const form = document.forms['contact_data_form'];
+const successPopup = document.getElementById('successPopup');
+const failedPopup = document.getElementById('failed_popup');
 
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            if (response.ok) {
+                // Show success popup
+                successPopup.classList.remove('d-none');
+                // Hide failed popup if it was shown previously
+                failedPopup.classList.add('d-none');
+            } else {
+                // Show failed popup
+                failedPopup.classList.remove('d-none');
+                // Hide success popup if it was shown previously
+                successPopup.classList.add('d-none');
+            }
+            // Reload the form after a delay
+            setTimeout(() => {
+                window.location.reload();
+            }, 1000); // Adjust the delay as needed
+        })
+        .catch(error => window.error('Error!', error.message));
+});
 
-// function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//     } else {
-//         x.className = "topnav";
-//     }
-// }
+// Function to close the success and failed popups
+function closePopup() {
+    successPopup.classList.add('d-none');
+    failedPopup.classList.add('d-none');
+};
